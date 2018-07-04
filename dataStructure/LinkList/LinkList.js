aa969654ca202e7d6e7c9fd12716bb63d8cbdcb0
KyleLan329 <kunlan3-c@my.cityu.edu.hk>
2018-07-04 19:35:37 +0800

    update LinkList

--- dataStructure/LinkList/LinkList.js
+++ dataStructure/LinkList/LinkList.js
@@ -5,4 +5,33 @@ export default class LinkList extends LinkListNode {
         this.head = null;
         this.tail = null;
     }
+
+    prepend(value) {
+        let newNode = new LinkListNode(value,this.head);
+        this.head = newNode;
+        
+        if (!this.tail) {
+            this.tail = newNode;
+        }
+        return;
+    }
+
+    append(value) {
+        let newNode = new LinkListNode(value);
+        if (!this.head) {
+            this.head = newNode;
+            this.tail = newNode;
+
+            return this;
+        }        
+
+        this.tail.next = newNode;
+        this.tail = newNode;
+
+        return this;
+    }
+
+    find(value) {
+        
+    }
 }
