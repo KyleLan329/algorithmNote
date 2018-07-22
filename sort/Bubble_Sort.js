1a84d5cc033610b96472a03a97f52a8bab660e37
KyleLan <kunlan3-c@my.cityu.edu.hk>
2018-07-22 23:31:53 +0800

    Update Bubble_Sort.js

--- sort/Bubble_Sort.js
+++ sort/Bubble_Sort.js
@@ -2,9 +2,9 @@ const swap = require('../utils/swap');
 
 function Bubble_Sort(arr) {
     let newArr = [...arr];
-    arr.forEach((item, index) => {
-        newArr[index] = item;
-    });
+    //arr.forEach((item, index) => {
+    //    newArr[index] = item;
+    // });
     let len = newArr.length;
     for ( let i = 0; i < len - 1; i++) {
         for (let j = i + 1; j < len; j++) {
